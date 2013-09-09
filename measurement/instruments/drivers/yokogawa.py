342ada2a50294e40518adadbf5f94c8143f36aa3
teamHQC <teamhqc@gmail.com>
2013-09-09 18:50:19 +0200

    Adding some checks and on/off method in yokoGS200 driver

--- measurement/instruments/drivers/yokogawa.py
+++ measurement/instruments/drivers/yokogawa.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 """
 """
-from visa import Instrument, VisaIOError
+from visa import Instrument, VisaIOError, VisaTypeError
+import re
+from textwrap import fill
 
 class YokogawaGS200(Instrument):
     """
@@ -27,15 +29,37 @@ class YokogawaGS200(Instrument):
     def set_function(self, mode):
         """
         """
-        if mode == 'VOLT':
+        volt = re.compile('VOLT', re.IGNORECASE)
+        curr = re.compile('CURR', re.IGNORECASE)
+        if volt.match(mode):
             self.write(':SOURce:FUNCtion VOLT')
             value = self.ask('SOURce:FUNCtion?')
             if value != 'VOLT':
                 raise VisaIOError('Instrument did not set correctly the mode')
-        elif mode == 'CURR':
+        elif curr.match(mode):
             self.write(':SOURce:FUNCtion CURR')
             value = self.ask('SOURce:FUNCtion?')
             if value != 'CURR':
                 raise VisaIOError('Instrument did not set correctly the mode')
         else:
-            print 'Mode unsupported for the YokogawaGS200'
+            mess = fill('''The invalid value {} was sent to set_function
+                        method of the Yokogawa driver'''.format(value), 80)
+            raise VisaTypeError(mess)
+            
+    def switch_on_off(self, value):
+        """
+        """
+        on = re.compile('on', re.IGNORECASE)
+        off = re.compile('off', re.IGNORECASE)
+        if on.match(value):
+            self.write(':OUTPUT ON')
+            if self.ask(':OUTPUT?')!= '1':
+                raise VisaIOError('Instrument did not set correctly the output')
+        elif off.match(value):
+            self.write(':OUTPUT OFF')
+            if self.ask(':OUTPUT?')!= '0':
+                raise VisaIOError('Instrument did not set correctly the output')
+        else:
+            mess = fill('''The invalid value {} was sent to switch_on_off
+                        method of the Yokogawa driver'''.format(value), 80)
+            raise VisaTypeError(mess)
