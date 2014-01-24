d4526783a4a7832de7489c36fd640f7f5ee06ee6
Matthieu Dartiailh <marul@laposte.net>
2014-01-24 19:07:32 +0100

    wip

--- hqc_meas/control/driver_debug_panel.py
+++ hqc_meas/control/driver_debug_panel.py
@@ -1,26 +1,32 @@
 # -*- coding: utf-8 -*-
 
-from atom.api import Atom, List, Dict, Str, Callable, Bool, Enum
+from atom.api import (Atom, List, Dict, Str, Callable, Bool, Unicode,
+                      Instance, Typed, Value)
+from inspect import getmembers, getabsfile, ismethod               
+                      
+from ..atom_util import Subclass
+from ..instruments.drivers import BaseInstrument, DRIVERS, DRIVER_TYPES
+from ..instruments.drivers.driver_tools import instrument_property
+from ..instruments.instrument_form import InstrumentForm
+from ..instrument.instrument_manager import matching_instr_list
 
 class DriverDebugPanel(Atom):
-    
-    driver_types
-    driver_type
 
-    drivers
-    driver
+    drivers = Dict(Str(), Subclass(BaseInstrument), DRIVERS)
+    driver = Subclass(BaseInstrument)
     
-    driver_properties
-    driver_methods
+    driver_properties = List(Str())
+    driver_methods = List(Callable())
     
-    profiles
-    profile
+    profiles = Dict(Str(), Unicode())
+    profile = Value()
+    custom_form = Typed(InstrumentForm, kwargs = {'name' :'Debug'})
 
-    connected
-    driver_ready
-    mode
+    connected = Bool()
+    driver_ready = Bool()
+    errors = Str()
     
-    _driver_instance
+    _driver_instance = Instance(BaseInstrument)
         
     def start_driver(self):
         """
@@ -50,14 +56,54 @@ class DriverDebugPanel(Atom):
     def attempt_get(self, prop):
         """
         """
-        pass
+        try:
+            val = getattr(self._driver_instance, prop)
+            return val
+        except Exception as e:
+            return e
     
     def attempt_set(self, prop, val):
         """
         """
-        pass
+        try:
+            setattr(self._driver_instance, prop, val)
+            return True
+        except Exception as e:
+            return e
     
-    def attempt_call(self, meth):
+    def attempt_call(self, meth, args, kwargs):
         """
         """
-        pass
+        try:
+            res = meth(self._driver_instance, *args, **kwargs)
+            return res
+        except Exception as e:
+            return e
+    
+    def _observe_driver(self, change):
+        """
+        """
+        driver = change['value']
+        self.profiles = matching_instr_list(driver)
+        
+        # Updating the custom form
+        for d_name, d_type in DRIVER_TYPES.iteritems():
+            if issubclass(driver, d_type):
+                self.custom_form.driver_type = d_name
+                break
+        for d_name, d in DRIVERS.iteritems():
+            if d == driver:
+                self.custom_form.driver = d_name
+                break
+        
+        # Listing driver properties
+        self.driver_properties = [m.__name__ for m in getmembers(driver,
+                             lambda x : isinstance(x, instrument_property))]
+        
+        # Listing driver method
+        self.driver_methods = [meth for meth in getmembers(driver, ismethod)
+                                if meth.__name__ not in self.driver_properties
+                                or meth.__name__.startswith('_')]
+                                
+    def _observe_profile(self, change):
+        self.driver_ready = bool(change['value'])
