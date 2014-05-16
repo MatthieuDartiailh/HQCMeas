b14a6842ba78f243fddf8342a851e99d8a251e67
Matthieu Dartiailh <marul@laposte.net>
2014-05-16 20:07:38 +0200

    Minor fix in debugger view.

--- hqc_meas/instruments/drivers/dummies/panel_dummy.py
+++ hqc_meas/instruments/drivers/dummies/panel_dummy.py
@@ -9,7 +9,8 @@ class PanelTestDummy(DummyInstrument):
     def __init__(self,  connection_info, caching_allowed=True,
                  caching_permissions={}, auto_open=True):
         super(PanelTestDummy, self).__init__(connection_info, caching_allowed,
-                                             caching_permissions, auto_open)
+                                             {'dummy_cached_int': True},
+                                             auto_open)
         self._float = 0.0
         self._int = 0
         self._enum = ['ON', 'OFF']
@@ -51,6 +52,19 @@ class PanelTestDummy(DummyInstrument):
     def dummy_enum(self, val):
         if self.fail or val not in self._enum:
             raise InstrIOError('Wrong val for dummy_enum')
+        print 'dummy_enum :', val
+        self._enum_val = val
+
+    @instrument_property
+    def dummy_cached_int(self):
+        if self.fail:
+            raise InstrIOError('Failed to get dummy_cached_int')
+        return self._enum_val
+
+    @dummy_cached_int.setter
+    def dummy_cached_int(self, val):
+        if self.fail or type(val) != int:
+            raise InstrIOError('Wrong val for dummy_cached_int')
         print 'dummy_enum :', val
         self._enum_val = val
 
