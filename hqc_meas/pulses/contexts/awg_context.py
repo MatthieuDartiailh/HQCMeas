515acb13e973d9c6fca3cbb4dd5d40ad889ce070
unknown <hqc@hqcspin.lpa.ens.fr>
2016-02-12 12:10:18 +0100

    pulses : fix template saving dialog

--- hqc_meas/pulses/contexts/awg_context.py
+++ hqc_meas/pulses/contexts/awg_context.py
@@ -93,7 +93,7 @@ class AWGContext(BaseContext):
 
             if channeltype == 'A' and pulse.kind == 'Analogical':
                 array_analog[channel][start_index:stop_index] +=\
-                    np.rint(8191*waveform)
+                    np.require(np.rint(8191*waveform), np.uint16)
             elif channeltype == 'M1' and pulse.kind == 'Logical':
                 array_M1[channel][start_index:stop_index] += waveform
             elif channeltype == 'M2' and pulse.kind == 'Logical':
